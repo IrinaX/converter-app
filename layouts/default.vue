55c0014bee1f582a698df741f08cfde68fc46812
IrinaX <iksanova08@mail.ru>
2020-12-24 03:03:30 +0500

    ui

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  background: #F9FAFC;
+  font-family: 'Saira', Roboto, sans-serif;
+  font-size: 25px;
+
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
