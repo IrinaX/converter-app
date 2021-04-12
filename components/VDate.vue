d0963bec4862e11c431d73ba0f2d5433d9a7d33c
IrinaX <iksanova08@mail.ru>
2021-04-12 17:28:03 +0500

    .

--- components/VDate.vue
+++ components/VDate.vue
@@ -1,257 +1,256 @@
 <template>
-  <div class="date-wrap">
-    <div class="date">
-      <div class="date__item">
-        <div class="date__item-title">Год:</div>
-        <div class="date__item-btns">
-          <button type="button" class="btn btn_date"
-                  v-for="year in d_renderData.d_yearsArr"
-                  @click="d_activeBtn.year = year; changeDate('year',year)"
-                  :class="{active: d_activeBtn.year === year}"
-          >{{ year }}
-          </button>
+    <div class="date-wrap">
+        <div class="date">
+            <div class="date__item">
+                <div class="date__item-title">Год:</div>
+                <div class="date__item-btns">
+                    <button type="button" class="btn btn_date"
+                            v-for="year in d_renderData.d_yearsArr"
+                            @click="d_activeBtn.year = year; changeDate('year',year)"
+                            :class="{active: d_activeBtn.year === year}"
+                    >{{ year }}
+                    </button>
 
+                </div>
+            </div>
+            <div class="separator"></div>
+            <div class="date__item">
+                <div class="date__item-title">Месяц:</div>
+                <div class="date__item-btns">
+                    <button type="button" class="btn btn_date"
+                            v-for="month in d_renderData.d_monthsArr"
+                            @click="d_activeBtn.month = month; changeDate('month',month)"
+                            :class="{active: d_activeBtn.month === month}"
+                    >{{ month }}
+                    </button>
+                </div>
+            </div>
+            <div class="separator"></div>
+            <div class="date__item">
+                <div class="date__item-title">День:</div>
+                <div class="date__item-btns">
+                    <button type="button" class="btn btn_date"
+                            v-for="day in d_renderData.d_daysArr"
+                            @click="d_activeBtn.day = day; changeDate('day',day)"
+                            :class="{active: d_activeBtn.day === day}"
+                    >{{ day }}
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="separator"></div>
-      <div class="date__item">
-        <div class="date__item-title">Месяц:</div>
-        <div class="date__item-btns">
-          <button type="button" class="btn btn_date"
-                  v-for="month in d_renderData.d_monthsArr"
-                  @click="d_activeBtn.month = month; changeDate('month',month)"
-                  :class="{active: d_activeBtn.month === month}"
-          >{{ month }}
-          </button>
-        </div>
-      </div>
-      <div class="separator"></div>
-      <div class="date__item">
-        <div class="date__item-title">День:</div>
-        <div class="date__item-btns">
-          <button type="button" class="btn btn_date"
-                  v-for="day in d_renderData.d_daysArr"
-                  @click="d_activeBtn.day = day; changeDate('day',day)"
-                  :class="{active: d_activeBtn.day === day}"
-          >{{ day }}
-          </button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-import {mapGetters, mapActions} from "vuex";
-import moment from "moment";
+import {mapGetters, mapActions} from 'vuex';
+import moment from 'moment';
 
 export default {
-  name: "VDate",
-  data() {
-    return {
-      d_fullDate: null,
-      d_renderData: {
-        d_yearsArr: [2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006,
-          2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992],
-        d_monthsArr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        d_daysArr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-      },
-      d_activeBtn: {
-        year: null,
-        month: null,
-        day: null,
-      }
-    };
-  },
-  async mounted() {
+    name: 'VDate',
+    data() {
+        return {
+            d_fullDate: null,
+            d_renderData: {
+                d_yearsArr: [2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006,
+                    2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992],
+                d_monthsArr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                d_daysArr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+            },
+            d_activeBtn: {
+                year: null,
+                month: null,
+                day: null,
+            },
+        };
+    },
+    async mounted() {
 
-    if (localStorage.date) {
-      let parsedDate = JSON.parse(localStorage.date);
-      this.d_activeBtn = {
-        year: parsedDate.year,
-        month: parsedDate.month * 1,
-        day: parsedDate.day * 1,
-      };
-      this.a_setDate({
-        year: parsedDate.year,
-        month: parsedDate.month,
-        day: parsedDate.day,
-      });
-    } else {
-      this.d_activeBtn = {
-        year: this.g_date.year,
-        month: this.g_date.month * 1,
-        day: this.g_date.day * 1,
-      };
-      this.a_setDate({
-        year: this.d_activeBtn.year,
-        month: this.addZero(this.d_activeBtn.month),
-        day: this.addZero(this.d_activeBtn.day),
-      });
-    }
-    this.d_fullDate = this.setFullDate(this.g_date);
-    await this.validateDate();
-  },
-  computed: {
-    ...mapGetters([
-      "g_date",
-      "g_data",
-      "g_error",
-      "g_info",
-      "g_renderKey",
-      "g_activeCurrencies",
-    ])
-  },
-  methods: {
-    ...mapActions([
-      "a_setDate",
-      "a_fetchCurrencies",
-      "a_setInfo",
-      "a_setRenderKey",
-      "a_setActiveCurr",
-      "a_setCurrencyExistence",
-    ]),
-    addZero(date) {
-      if (date.toString().length < 2) {
-        return "0" + date.toString();
-      } else {
-        return date;
-      }
-    },
-    async changeDate(title, value) {
-      if (title === "year") {
-        this.a_setDate({
-          year: value,
-          month: this.d_activeBtn.month,
-          day: this.d_activeBtn.day,
-        });
-      }
-      if (title === "month") {
-        this.a_setDate({
-          year: this.d_activeBtn.year,
-          month: value,
-          day: this.d_activeBtn.day,
-        });
-      }
-      if (title === "day") {
-        this.a_setDate({
-          year: this.d_activeBtn.year,
-          month: this.d_activeBtn.month,
-          day: value,
-        });
-      }
-      this.d_fullDate = this.setFullDate(this.g_date);
-      await this.validateDate();
-    },
-    async validateDate() {
-      let currDate = {
-        year: new Date().getFullYear(),
-        month: new Date().getMonth() + 1,
-        day: new Date().getDate(),
-      };
-      if (this.g_date.year == currDate.year && (this.g_date.month > currDate.month || (this.g_date.day >= currDate.day && this.g_date.month == currDate.month))) {
-        this.a_setInfo("Данные на " + this.setFullDate(currDate) + ":");
-        await this.a_fetchCurrencies("https://www.cbr-xml-daily.ru/daily_json.js");
-      } else {
-        if (this.g_date.year == 1992 && (this.g_date.month < 7 || (this.g_date.day === 1 && this.g_date.month === 7))) {
-          this.a_setInfo("Данные на 1992/07/01:");
-          await this.a_fetchCurrencies("https://www.cbr-xml-daily.ru/archive/1992/07/01/daily_json.js");
+        if (localStorage.date) {
+            let parsedDate = JSON.parse(localStorage.date);
+            this.d_activeBtn = {
+                year: parsedDate.year,
+                month: parsedDate.month * 1,
+                day: parsedDate.day * 1,
+            };
+            this.a_setDate({
+                year: parsedDate.year,
+                month: parsedDate.month,
+                day: parsedDate.day,
+            });
         } else {
-          await this.findUrl();
+            this.d_activeBtn = {
+                year: this.g_date.year,
+                month: this.g_date.month * 1,
+                day: this.g_date.day * 1,
+            };
+            this.a_setDate({
+                year: this.d_activeBtn.year,
+                month: this.addZero(this.d_activeBtn.month),
+                day: this.addZero(this.d_activeBtn.day),
+            });
         }
-      }
-      this.g_activeCurrencies.forEach((itm, i) => {
-        let index = i;
-        if (this.g_data.Valute[itm.CharCode]) {
-          let item = this.g_data.Valute[itm.CharCode];
-          this.a_setActiveCurr({item, index});
-          let condition = true;
-          this.a_setCurrencyExistence({condition, index});
-        } else {
-          let condition = false;
-          this.a_setCurrencyExistence({condition, index});
-        }
-      });
-      await this.a_setRenderKey(Date.now().toString());
+        this.d_fullDate = this.setFullDate(this.g_date);
+        await this.validateDate();
     },
-    setFullDate(date, separator = "/") {
-      let year = date.year;
-      let month = this.addZero(date.month);
-      let day = this.addZero(date.day);
-      return year + separator + month + separator + day;
+    computed: {
+        ...mapGetters([
+            'g_date',
+            'g_data',
+            'g_error',
+            'g_info',
+            'g_renderKey',
+            'g_activeCurrencies',
+        ]),
     },
-    async findUrl() {
-      await this.a_fetchCurrencies("https://www.cbr-xml-daily.ru/archive/" + this.d_fullDate + "/daily_json.js");
-      if (this.g_error === false) {
-        this.a_setInfo("Данные на " + this.d_fullDate + ":");
-      } else {
-        let momentDate = this.g_date;
-        let oldFullDate = this.d_fullDate;
-        while (this.g_error) {
-          momentDate = moment(this.setFullDate(momentDate, "-")).subtract(1, "days").format("L");
-          momentDate = {
-            year: momentDate.slice(6, 10),
-            month: momentDate.slice(0, 2),
-            day: momentDate.slice(3, 5),
-          };
-          this.d_fullDate = this.setFullDate(momentDate);
-          await this.a_fetchCurrencies("https://www.cbr-xml-daily.ru/archive/" + this.d_fullDate + "/daily_json.js");
-        }
-        this.a_setInfo("Данные на " + oldFullDate + " отсутствуют. " + "Показаны данные на " + this.d_fullDate + ":");
-      }
-    }
-  },
+    methods: {
+        ...mapActions([
+            'a_setDate',
+            'a_fetchCurrencies',
+            'a_setInfo',
+            'a_setRenderKey',
+            'a_setActiveCurr',
+            'a_setCurrencyExistence',
+        ]),
+        addZero(date) {
+            if (date.toString().length < 2) {
+                return '0' + date.toString();
+            } else {
+                return date;
+            }
+        },
+        async changeDate(title, value) {
+            if (title === 'year') {
+                this.a_setDate({
+                    year: value,
+                    month: this.d_activeBtn.month,
+                    day: this.d_activeBtn.day,
+                });
+            }
+            if (title === 'month') {
+                this.a_setDate({
+                    year: this.d_activeBtn.year,
+                    month: value,
+                    day: this.d_activeBtn.day,
+                });
+            }
+            if (title === 'day') {
+                this.a_setDate({
+                    year: this.d_activeBtn.year,
+                    month: this.d_activeBtn.month,
+                    day: value,
+                });
+            }
+            this.d_fullDate = this.setFullDate(this.g_date);
+            await this.validateDate();
+        },
+        async validateDate() {
+            let currDate = {
+                year: new Date().getFullYear(),
+                month: new Date().getMonth() + 1,
+                day: new Date().getDate(),
+            };
+            if (this.g_date.year == currDate.year && (this.g_date.month > currDate.month || (this.g_date.day >= currDate.day && this.g_date.month == currDate.month))) {
+                this.a_setInfo('Данные на ' + this.setFullDate(currDate) + ':');
+                await this.a_fetchCurrencies('https://www.cbr-xml-daily.ru/daily_json.js');
+            } else {
+                if (this.g_date.year == 1992 && (this.g_date.month < 7 || (this.g_date.day === 1 && this.g_date.month === 7))) {
+                    this.a_setInfo('Данные на 1992/07/01:');
+                    await this.a_fetchCurrencies('https://www.cbr-xml-daily.ru/archive/1992/07/01/daily_json.js');
+                } else {
+                    await this.findUrl();
+                }
+            }
+            this.g_activeCurrencies.forEach((itm, i) => {
+                let index = i;
+                if (this.g_data.Valute[itm.CharCode]) {
+                    let item = this.g_data.Valute[itm.CharCode];
+                    this.a_setActiveCurr({item, index});
+                    let condition = true;
+                    this.a_setCurrencyExistence({condition, index});
+                } else {
+                    let condition = false;
+                    this.a_setCurrencyExistence({condition, index});
+                }
+            });
+            await this.a_setRenderKey(Date.now().toString());
+        },
+        setFullDate(date, separator = '/') {
+            let year = date.year;
+            let month = this.addZero(date.month);
+            let day = this.addZero(date.day);
+            return year + separator + month + separator + day;
+        },
+        async findUrl() {
+            await this.a_fetchCurrencies('https://www.cbr-xml-daily.ru/archive/' + this.d_fullDate + '/daily_json.js');
+            if (this.g_error === false) {
+                this.a_setInfo('Данные на ' + this.d_fullDate + ':');
+            } else {
+                let momentDate = this.g_date;
+                let oldFullDate = this.d_fullDate;
+                while (this.g_error) {
+                    momentDate = moment(this.setFullDate(momentDate, '-')).subtract(1, 'days').format('L');
+                    momentDate = {
+                        year: momentDate.slice(6, 10),
+                        month: momentDate.slice(0, 2),
+                        day: momentDate.slice(3, 5),
+                    };
+                    this.d_fullDate = this.setFullDate(momentDate);
+                    await this.a_fetchCurrencies('https://www.cbr-xml-daily.ru/archive/' + this.d_fullDate + '/daily_json.js');
+                }
+                this.a_setInfo('Данные на ' + oldFullDate + ' отсутствуют. ' + 'Показаны данные на ' + this.d_fullDate + ':');
+            }
+        },
+    },
 };
 </script>
 
 <style scoped lang="scss">
 .date {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  flex-direction: column;
-
-  .date__item {
     display: flex;
-    justify-content: flex-start;
     align-items: center;
-    width: 100%;
-  }
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
 
-  .date__item-title {
-    font-size: 18px;
-    padding-right: 10px;
-  }
-
-  .date__item-btns {
-    width: 100%;
-    max-height: 44px;
-    overflow-x: scroll;
-    overflow-y: hidden;
-    display: flex;
-    justify-content: flex-start;
-
-    &::-webkit-scrollbar {
-      width: auto;
-      height: 4px;
+    .date__item {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        width: 100%;
     }
 
-    &::-webkit-scrollbar-track {
-      background: transparent;
+    .date__item-title {
+        font-size: 18px;
+        padding-right: 10px;
     }
 
-    &::-webkit-scrollbar-thumb {
-      background-color: #d2d2d2;
-      border-radius: 20px;
+    .date__item-btns {
+        width: 100%;
+        overflow-x: scroll;
+        overflow-y: hidden;
+        display: flex;
+        justify-content: flex-start;
+
+        &::-webkit-scrollbar {
+            width: auto;
+            height: 4px;
+        }
+
+        &::-webkit-scrollbar-track {
+            background: transparent;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background-color: #d2d2d2;
+            border-radius: 20px;
+        }
+
     }
 
-  }
-
-  .separator {
-    width: 100%;
-    height: 2px;
-    background-color: rgba($primary, .5);
-  }
+    .separator {
+        width: 100%;
+        height: 2px;
+        background-color: rgba($primary, .5);
+    }
 }
 </style>
--- store/index.js
+++ store/index.js
@@ -1,178 +1,179 @@
 export const state = () => ({
-  s_isModalActive: false,
-  s_data: null,
-  s_error: null,
-  s_clickedCurrencyIndex: null,
-  s_clickedInputIndex: null,
-  s_activeCurrencies: [
-    // {
-    //   CharCode: "GBP",
-    //   ID: "R01035",
-    //   Name: "Фунт стерлингов Соединенного королевства",
-    //   Nominal: 1,
-    //   NumCode: "826",
-    //   Previous: 101.6017,
-    //   Value: 99.9265,
-    // },
-    // {
-    //   CharCode: "AUD",
-    //   ID: "R01010",
-    //   Name: "Австралийский доллар",
-    //   Nominal: 1,
-    //   NumCode: "036",
-    //   Previous: 57.0229,
-    //   Value: 56.8404,
-    // }
-  ],
-  s_currencyExistence: {
-    first: null,
-    second: null
-  },
-  s_firstCurrVal: null,
-  s_secondCurrVal: null,
-  s_date: {
-    year: new Date().getFullYear(),
-    month: new Date().getMonth() + 1,
-    day: new Date().getDate(),
-  },
-  s_info: null,
-  s_renderKey: Date.now().toString()
+    s_isModalActive: false,
+    s_data: null,
+    s_error: null,
+    s_clickedCurrencyIndex: null,
+    s_clickedInputIndex: null,
+    s_activeCurrencies: [
+        // {
+        //   CharCode: "GBP",
+        //   ID: "R01035",
+        //   Name: "Фунт стерлингов Соединенного королевства",
+        //   Nominal: 1,
+        //   NumCode: "826",
+        //   Previous: 101.6017,
+        //   Value: 99.9265,
+        // },
+        // {
+        //   CharCode: "AUD",
+        //   ID: "R01010",
+        //   Name: "Австралийский доллар",
+        //   Nominal: 1,
+        //   NumCode: "036",
+        //   Previous: 57.0229,
+        //   Value: 56.8404,
+        // }
+    ],
+    s_currencyExistence: {
+        first: null,
+        second: null,
+    },
+    s_firstCurrVal: null,
+    s_secondCurrVal: null,
+    s_date: {
+        year: new Date().getFullYear(),
+        month: new Date().getMonth() + 1,
+        day: new Date().getDate(),
+    },
+    s_info: null,
+    s_renderKey: Date.now().toString(),
 });
 
 export const mutations = {
-  m_fetchCurrencies(state, data) {
-    state.s_data = data;
-  },
-  m_isModalActive(state) {
-    state.s_isModalActive = !state.s_isModalActive;
-  },
-  m_clickedCurrencyIndex(state, index) {
-    state.s_clickedCurrencyIndex = index;
-  },
-  m_clickedInputIndex(state, index) {
-    state.s_clickedInputIndex = index;
-  },
-  m_activeCurrencies(state, {item, index}) {
-    state.s_activeCurrencies[index] = item;
-    if (window.localStorage.activeCurrencies){
-      window.localStorage.activeCurrencies = JSON.stringify(state.s_activeCurrencies);
-    }else{
-      window.localStorage.setItem('activeCurrencies',JSON.stringify(state.s_activeCurrencies))
-    }
-  },
-  m_currencyExistence(state, {condition, index}) {
-    if (index == 0){
-      state.s_currencyExistence.first = condition;
-    } else {
-      state.s_currencyExistence.second = condition;
-    }
-    // if (window.localStorage.currencyExistence){
-    //   window.localStorage.currencyExistence = JSON.stringify(state.s_currencyExistence);
-    // }else{
-    //   window.localStorage.setItem('currencyExistence',JSON.stringify(state.s_currencyExistence))
-    // }
-  },
-  m_firstCurrVal(state, val) {
-    state.s_firstCurrVal = val;
-    if (window.localStorage.firstCurrVal){
-      window.localStorage.firstCurrVal = val;
-    }else{
-      window.localStorage.setItem('firstCurrVal',JSON.stringify(val))
-    }
-  },
-  m_secondCurrVal(state, val) {
-    state.s_secondCurrVal = val;
-    if (window.localStorage.secondCurrVal){
-      window.localStorage.secondCurrVal = val;
-    }else{
-      window.localStorage.setItem('secondCurrVal',JSON.stringify(val))
-    }
-  },
-  m_date(state, date) {
-    state.s_date = date;
-    if (window.localStorage.date){
-      window.localStorage.date = JSON.stringify(date);
-    }else{
-      window.localStorage.setItem('date',JSON.stringify(date))
-    }
-  },
-  m_error(state, condition) {
-    state.s_error = condition;
-  },
-  m_info(state, text) {
-    state.s_info = text;
-  },
-  m_RenderKey(state, key) {
-    state.s_renderKey = key;
-  }
+    m_fetchCurrencies(state, data) {
+        state.s_data = data;
+    },
+    m_isModalActive(state) {
+        state.s_isModalActive = !state.s_isModalActive;
+    },
+    m_clickedCurrencyIndex(state, index) {
+        state.s_clickedCurrencyIndex = index;
+    },
+    m_clickedInputIndex(state, index) {
+        state.s_clickedInputIndex = index;
+    },
+    m_activeCurrencies(state, {item, index}) {
+        state.s_activeCurrencies[index] = item;
+        if (window.localStorage.activeCurrencies) {
+            window.localStorage.activeCurrencies = JSON.stringify(state.s_activeCurrencies);
+        } else {
+            window.localStorage.setItem('activeCurrencies', JSON.stringify(state.s_activeCurrencies));
+        }
+    },
+    m_currencyExistence(state, {condition, index}) {
+        if (index == 0) {
+            state.s_currencyExistence.first = condition;
+        } else {
+            state.s_currencyExistence.second = condition;
+        }
+        // if (window.localStorage.currencyExistence){
+        //   window.localStorage.currencyExistence = JSON.stringify(state.s_currencyExistence);
+        // }else{
+        //   window.localStorage.setItem('currencyExistence',JSON.stringify(state.s_currencyExistence))
+        // }
+    },
+    m_firstCurrVal(state, val) {
+        state.s_firstCurrVal = val;
+        if (window.localStorage.firstCurrVal) {
+            window.localStorage.firstCurrVal = val;
+        } else {
+            window.localStorage.setItem('firstCurrVal', JSON.stringify(val));
+        }
+    },
+    m_secondCurrVal(state, val) {
+        state.s_secondCurrVal = val;
+        if (window.localStorage.secondCurrVal) {
+            window.localStorage.secondCurrVal = val;
+        } else {
+            window.localStorage.setItem('secondCurrVal', JSON.stringify(val));
+        }
+    },
+    m_date(state, date) {
+        state.s_date = date;
+        if (window.localStorage.date) {
+            window.localStorage.date = JSON.stringify(date);
+        } else {
+            window.localStorage.setItem('date', JSON.stringify(date));
+        }
+    },
+    m_error(state, condition) {
+        state.s_error = condition;
+    },
+    m_info(state, text) {
+        state.s_info = text;
+    },
+    m_RenderKey(state, key) {
+        state.s_renderKey = key;
+    },
 };
 
 export const actions = {
-  async a_fetchCurrencies(ctx, url) {
-    let response = await fetch(url)
-      .then(res => {
-        if (res.ok) {
-          return res;
-        } else {
-          throw new Error(res.statusText);
-        }
-      })
-      .then((res) => {
-        ctx.dispatch("a_error", false);
-        return res.json();
-      })
-      .catch((e) => {
-        ctx.dispatch("a_error", true);
-        return console.log(e);
-      });
-    ctx.commit("m_fetchCurrencies", response);
-  },
-  a_error({commit}, condition) {
-    commit("m_error", condition);
-  },
-  a_toggleModal({commit}) {
-    commit("m_isModalActive");
-  },
-  a_clickedCurrencyIndex({commit}, index) {
-    commit("m_clickedCurrencyIndex", index);
-  },
-  a_clickedInputIndex({commit}, index) {
-    commit("m_clickedInputIndex", index);
-  },
-  a_setCurrencyExistence({commit}, {condition, index}) {
-      commit("m_currencyExistence", {condition, index});
-  },
-  a_setActiveCurr({commit}, {item, index}) {
-    commit("m_activeCurrencies", {item, index});
-  },
-  a_setFirstCurrVal({commit}, val) {
-    commit("m_firstCurrVal", val);
-  },
-  a_setSecondCurrVal({commit}, val) {
-    commit("m_secondCurrVal", val);
-  },
-  a_setDate({commit}, date) {
-    commit("m_date", date);
-  },
-  a_setInfo({commit}, text) {
-    commit("m_info", text);
-  },
-  a_setRenderKey({commit}, key) {
-    commit("m_RenderKey", key);
-  }
+    async a_fetchCurrencies(ctx, url) {
+        let headers = new Headers();
+        let response = await fetch(url, {})
+            .then(res => {
+                if (res.ok) {
+                    return res;
+                } else {
+                    throw new Error(res.statusText);
+                }
+            })
+            .then((res) => {
+                ctx.dispatch('a_error', false);
+                return res.json();
+            })
+            .catch((e) => {
+                ctx.dispatch('a_error', true);
+                return console.log(e);
+            });
+        ctx.commit('m_fetchCurrencies', response);
+    },
+    a_error({commit}, condition) {
+        commit('m_error', condition);
+    },
+    a_toggleModal({commit}) {
+        commit('m_isModalActive');
+    },
+    a_clickedCurrencyIndex({commit}, index) {
+        commit('m_clickedCurrencyIndex', index);
+    },
+    a_clickedInputIndex({commit}, index) {
+        commit('m_clickedInputIndex', index);
+    },
+    a_setCurrencyExistence({commit}, {condition, index}) {
+        commit('m_currencyExistence', {condition, index});
+    },
+    a_setActiveCurr({commit}, {item, index}) {
+        commit('m_activeCurrencies', {item, index});
+    },
+    a_setFirstCurrVal({commit}, val) {
+        commit('m_firstCurrVal', val);
+    },
+    a_setSecondCurrVal({commit}, val) {
+        commit('m_secondCurrVal', val);
+    },
+    a_setDate({commit}, date) {
+        commit('m_date', date);
+    },
+    a_setInfo({commit}, text) {
+        commit('m_info', text);
+    },
+    a_setRenderKey({commit}, key) {
+        commit('m_RenderKey', key);
+    },
 };
 
 export const getters = {
-  g_isModalActive: s => s.s_isModalActive,
-  g_data: s => s.s_data,
-  g_activeCurrencies: s => s.s_activeCurrencies,
-  g_clickedCurrencyIndex: s => s.s_clickedCurrencyIndex,
-  g_clickedInputIndex: s => s.s_clickedInputIndex,
-  g_currencyExistence: s => s.s_currencyExistence,
-  g_firstCurrVal: s => s.s_firstCurrVal,
-  g_secondCurrVal: s => s.s_secondCurrVal,
-  g_date: s => s.s_date,
-  g_error: s => s.s_error,
-  g_info: s => s.s_info,
-  g_renderKey: s => s.s_renderKey,
+    g_isModalActive: s => s.s_isModalActive,
+    g_data: s => s.s_data,
+    g_activeCurrencies: s => s.s_activeCurrencies,
+    g_clickedCurrencyIndex: s => s.s_clickedCurrencyIndex,
+    g_clickedInputIndex: s => s.s_clickedInputIndex,
+    g_currencyExistence: s => s.s_currencyExistence,
+    g_firstCurrVal: s => s.s_firstCurrVal,
+    g_secondCurrVal: s => s.s_secondCurrVal,
+    g_date: s => s.s_date,
+    g_error: s => s.s_error,
+    g_info: s => s.s_info,
+    g_renderKey: s => s.s_renderKey,
 };
